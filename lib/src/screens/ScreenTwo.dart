360f36fa9b08dd0baeee5ce0c2e81c583002cec6
Argel  Bejarano <argel.bc18@gmail.com>
2019-01-14 19:34:05 -0700

    limpieza

--- lib/src/screens/ScreenTwo.dart
+++ lib/src/screens/ScreenTwo.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PantallaDos extends StatelessWidget {
+class ScreenTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
