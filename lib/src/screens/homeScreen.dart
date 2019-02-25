12050296fd776095f0e393d8817d3cb3d903c07d
Argel  Bejarano <argel.bc18@gmail.com>
2019-02-25 09:51:15 -0700

    se limpio estructura basica

--- lib/src/screens/homeScreen.dart
+++ lib/src/screens/homeScreen.dart
@@ -1,23 +1,11 @@
 import 'package:flutter/material.dart';
-
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key, this.title}) : super(key: key);
-
   final String title;
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
-
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,11 +13,9 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Container()
-      ),
+      body: Center(child: Text('Hola')),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: null,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
